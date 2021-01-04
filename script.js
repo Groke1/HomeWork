f7ecb67e836edeeec296c612453238a435d6d3cf
Gregory <gregoryr-2005@mail.ru>
2021-01-04 05:20:06 +0300

    add firstcycle

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
-'use strict'
-let money = prompt("Ваш бюджет на месяц?", "");
+let money = +prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-let expenses = {},
-    optionalExpenses = {},
-    income = [],
-    savings = false;
+let appData = {
+    budget: money,
+    expences: {},
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
 
 for (let i = 0; i < 2; i++) {
     let cause = prompt("Введите обязательную статью расходов в этом месяце"); 
     let price = prompt("Во сколько обойдется?");
-    expenses.cause = price;
+
+    if  ( (typeof(cause) === 'string') && (typeof(cause) != null) 
+        && (typeof(price) === 'string') && (typeof(price) != null)
+        && cause != '' && price != '' && cause.length < 50){
+        appData.expences[cause] = price;
+    };
 };
 
-let appData = {
-    money: money,
-    time: time,
-    expences: expenses,
-    optionalExpenses: optionalExpenses,
-    income: income,
-    savings: savings
-};
+appData.moneyPerDay = appData.moneyPerDay/30;
 
-console.log(appData);
-alert("Ваш бюджет на 1 день: " + money/30);
+alert("Ваш бюджет на 1 день: " + moneyPerDay);
+
+if (appData.moneyPerDay < 100){
+    console.log("Минимальный уровень достатка");
+} else if (2000 > appData.moneyPerDay > 100) {
+    console.log("Средний уровень достатка")
+}
